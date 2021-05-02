4c7376eb7a00e45fa983aebc41a2d29061fe4467
sandestareza <sandesta24@gmail.com>
2021-05-02 14:19:58 +0700

    Upload file

---
+++ vendor/js/script.js
@@ -0,0 +1,28 @@
+// responsif web
+$(document).ready(function () {
+    $(window).resize(function () {
+        var vWidth = $(window).width()
+        if (vWidth <= 992) {
+            $('.col-md-7').addClass('order-last')
+            $('.col-md-5').addClass('order-first')
+        } else {
+            $('.col-md-7').removeClass('order-last')
+            $('.col-md-5').removeClass('order-first')
+        }
+    })
+})
+
+// nav page active
+$('body').scrollspy({
+    target: '#navbarNavDropdown',
+    offset: 100
+})
+
+$('a').smoothScroll({
+    speed: 800
+});
+
+// animasi scroll
+$(document).ready(function () {
+    AOS.init();
+})
